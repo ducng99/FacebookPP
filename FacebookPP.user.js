50aa32615fb42149b90fba4db86a4b9d58ad7a4f
Duc Nguyen <49080794+ducng99@users.noreply.github.com>
2019-08-18 04:02:04 +1200

    No 5s sleep

--- FacebookPP.user.js
+++ FacebookPP.user.js
@@ -14,47 +14,57 @@
     var theScript;
     var vidURL = "#";
 
-    setTimeout(function() {
-        var scriptTags = jQuery("script");
-
-        for (var i = 0; i < scriptTags.length; i++)
+    (function CheckLoop(foundScript) {
+        setTimeout(function()
         {
-            if (scriptTags[i].innerHTML.includes(".mp4"))
+            var scriptTags = jQuery("script");
+
+            for (var i = 0; i < scriptTags.length; i++)
             {
-                theScript = scriptTags[i].innerHTML;
-                break;
-            }
-        }
-
-        if (theScript.indexOf("hd_src\":null") != -1)
-        {
-            vidURL = theScript.substring(theScript.indexOf("sd_src") + 9, theScript.indexOf("\",\"hd_tag"));
-            vidURL = vidURL.replace(/\\\//g, "/");
-        }
-        else if (theScript.indexOf("hd_src\":\"") != -1)
-        {
-            vidURL = theScript.substring(theScript.indexOf("hd_src") + 9, theScript.indexOf("\",\"sd_src"));
-            vidURL = vidURL.replace(/\\\//g, "/");
-        }
-        else if (theScript.indexOf(",hd_src:") != -1)
-        {
-            vidURL = theScript.substring(theScript.indexOf("hd_src") + 8, theScript.indexOf("\",sd_src:"));
-        }
-        
-        (function FindMenu(found)
-        {
-            setTimeout(function() {
-                if (jQuery(".uiContextualLayer").length)
+                if (scriptTags[i].innerHTML.includes(".mp4"))
                 {
-                    found = true;
-        
-                    var DownloadButton = jQuery("<li class=\"_54ni __MenuItem\" role=\"presentation\"><a class=\"_54nc\" href=\"" + vidURL + "\" role=\"menuitem\"><span><span class=\"_54nh\">Download</span></span></a></li>");
-                    jQuery("._54nf").append(DownloadButton);
+                    foundScript = true;
+                    theScript = scriptTags[i].innerHTML;
+                    break;
                 }
-                
-                if (!found)
-                    FindMenu(found);
-            }, 200);
-        })(false);
-    }, 5000);
+            }
+            
+            if (!foundScript)
+                CheckLoop(foundScript);
+            else
+            {
+                if (theScript.indexOf("hd_src\":null") != -1)
+                {
+                    vidURL = theScript.substring(theScript.indexOf("sd_src") + 9, theScript.indexOf("\",\"hd_tag"));
+                    vidURL = vidURL.replace(/\\\//g, "/");
+                }
+                else if (theScript.indexOf("hd_src\":\"") != -1)
+                {
+                    vidURL = theScript.substring(theScript.indexOf("hd_src") + 9, theScript.indexOf("\",\"sd_src"));
+                    vidURL = vidURL.replace(/\\\//g, "/");
+                }
+                else if (theScript.indexOf(",hd_src:") != -1)
+                {
+                    vidURL = theScript.substring(theScript.indexOf("hd_src") + 8, theScript.indexOf("\",sd_src:"));
+                }
+
+                (function FindMenu(found)
+                 {
+                    setTimeout(function() {
+                        if (jQuery(".uiContextualLayer").length)
+                        {
+                            found = true;
+
+                            var DownloadButton = jQuery("<li class=\"_54ni __MenuItem\" role=\"presentation\"><a class=\"_54nc\" href=\"" + vidURL + "\" role=\"menuitem\"><span><span class=\"_54nh\">Download</span></span></a></li>");
+                            jQuery("._54nf").append(DownloadButton);
+                        }
+
+                        if (!found)
+                            FindMenu(found);
+                    }, 200);
+                })(false);
+            }
+        }, 100);
+    })(false);
+    
 })();
