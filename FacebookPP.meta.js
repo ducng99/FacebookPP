a5f1209763b77df1ad0aaa7ecf8f77b6243c1304
Duc Nguyen <tuanduc249.td@gmail.com>
2020-12-15 23:25:01 +1300

    Create FacebookPP.meta.js

---
+++ FacebookPP.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Facebook++
+// @namespace    maxhyt.fbpp
+// @version      3.1.0
+// @description  download vid & block ads
+// @author       Maxhyt
+// @updateURL    https://ducng99.github.io/FacebookPP/FacebookPP.meta.js
+// @downloadURL  https://ducng99.github.io/FacebookPP/FacebookPP.user.js
+// ==/UserScript==
