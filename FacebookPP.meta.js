03655e745474b974b118a3a3b2be28b65577769f
Thomas Nguyen <tuanduc249.td@gmail.com>
2021-07-10 20:56:29 +1200

    Update FacebookPP.meta.js

--- FacebookPP.meta.js
+++ FacebookPP.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Facebook++
 // @namespace    maxhyt.fbpp
-// @version      3.1.1
+// @version      3.1.2
 // @description  download vid & block ads
 // @author       Maxhyt
 // @updateURL    https://ducng99.github.io/FacebookPP/FacebookPP.meta.js
